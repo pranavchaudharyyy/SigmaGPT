0070278b520d7892586f85684ccd15d4d5316e31
Pranav Chaudhary <pranav16288@gmail.com>
2025-12-31 23:51:54 -0800

    Fix Chat import path for Vercel build

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,5 +1,6 @@
 import "./ChatWindow.css";
-import Chat from "./chat";
+import Chat from "./chat.jsx";
+
 import { MyContext } from "./MyContext.jsx";
 import { useContext, useState, useEffect } from "react";
 import {ScaleLoader} from "react-spinners";
