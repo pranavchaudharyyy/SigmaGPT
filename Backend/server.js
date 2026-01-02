d7b2193c36f0bc9b42fca05a9c695b5aa2e6f2f5
Pranav Chaudhary <pranav16288@gmail.com>
2026-01-01 20:44:21 -0800

    Fix VS Code JSX import issue with jsconfig

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import chatRoutes from "./routes/chat.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
@@ -22,6 +22,27 @@ const connectDB = async() => {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected with Database!");
         console.log("API KEY LOADED:", process.env.OPENAI_API_KEY ? "YES" : "NO");
+
+
+
+
+
+
+
+        app.get("/api/test", (req, res) => {
+  res.send("Backend is connected!");
+});
+
+        
+
+
+
+
+
+
+
+
+
 
 
     } catch(err) {
